f3427013d8759535b4093396eafeb7ee09d8620c
Jonathan Lupton <jlupton@salesforce.com>
2020-02-22 19:54:45 -0500

    Added Flow for Fast Skill Data Entry Added Process Builder to Track History of Skill Over Time

--- mdapipkg/TerminalCommands.sh
+++ mdapipkg/TerminalCommands.sh
@@ -2,14 +2,21 @@ sfdx force:org:list
 sfdx force:mdapi:retrieve -s -r ./mdapipkg -u jlupton-xpnc@force.com -p Topics
 unzip ./mdapipkg/unpackaged.zip -d ./mdapipkg/source
 force:mdapi:convert --rootdir mdapipkg/source
+
 sfdx force:alias:list
 sfdx force:data:soql:query -q "SELECT Description__c,Name,Type__c FROM Topic__c" -u DevHub -r csv 
 sfdx force:data:bulk:upsert -s Topic__c -f ./data/TopicData.csv -i Id -u Topics
 
-SELECT Description__c,Private__c,Resource_Type__c,Source__c,Url__c FROM Digital_Resource__c
-SELECT Private__c,Resource__c,Topic__c FROM Topic_Resource__c
 
+git init
 git remote add origin https://github.com/luptongit/SalesforceInvokableFlow.git
+git add .
+git commit -m "initial commit"
+git push -u origin master --force
+
+sfdx force:org:open
+sfdx force:source:push
+sfdx force:source:pull
 
 sfdx force:data:tree:export -q "SELECT Name, Description__c, Type__c, 
 (SELECT Name,  Topic__c, Private__c, 
@@ -29,5 +36,6 @@ sfdx force:data:tree:export -q "SELECT Name, Description__c, Type__c FROM Topic_
 sfdx force:data:tree:export -q "SELECT Name, Resource_Type__c, Url__c, Private__c, Description__c, Source__c FROM Digital_Resource__c" -d ./data/json -u DevHub --prefix package_data --plan
 sfdx force:data:tree:export -q "SELECT Name, Resource__r.Id, Topic__r.Id, Private__c FROM Topic_Resource__c" -d ./data/json -u DevHub --prefix package_data --plan
 
-sfdx force:data:tree:import -u Topics --plan ./data/json/package_data-Topic__c-plan.json
+
+sfdx force:data:tree:import -u Topics --plan data/json/package_data-Digital_Resource__c-plan.json
 
