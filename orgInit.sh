e9a4ed5bfc378952f72e768665ff7d408c3edfc1
luptongit <jlupton@salesforce.com>
2021-03-11 21:07:28 -0500

    testing

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s -w 60
 sfdx force:source:push
 sfdx automig:load -d ./data/automig4
-sfdx force:org:open -p /lightning/o/Topic__c/list?filterName=Recent
+sfdx force:org:open 
